a089468dd2581749627bcea7ab7ac13ada2c311a
Cosmin Iordache <iordachecosmin28@gmail.com>
2023-06-07 17:02:39 +0300

    Implement Error handling

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -18,13 +18,13 @@ export default observer(function ActivityDashboard() {
     if (activityStore.loadingInitial) return (<LoadingComponent content='Loading app'/>)
 
     return (
-        <Grid>
-            <Grid.Column width='10'>
-                <ActivityList  />
-            </Grid.Column>
-            <Grid.Column width='6'>
-                <ActivityFilter/>
-            </Grid.Column>
-        </Grid>
+            <Grid>
+                <Grid.Column width='10'>
+                    <ActivityList  />
+                </Grid.Column>
+                <Grid.Column width='6'>
+                    <ActivityFilter/>
+                </Grid.Column>
+            </Grid>
     )
 })
