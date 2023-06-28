fc7cf8b02d2e745463021002193a3d0f0186b1ff
Cosmin Iordache <iordachecosmin28@gmail.com>
2023-06-28 16:50:10 +0300

    Client side attendance

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { Button, Header, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import {v4 as uuid} from 'uuid'
 import { Formik, Form } from "formik";
@@ -24,15 +24,7 @@ export default observer(function ActivityForm() {
     
     const {id} = useParams< {id:string }> ();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    })
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -44,16 +36,16 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
     
-    function handleFormSubmit(activity: Activity) {
-        if(activity.id.length === 0){
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if(!activity.id){
             let newActivity ={
                 ...activity,
                 id:uuid()
             };
-            createActivity(newActivity).then(() => navigate(`/activities/${activity.id}`));
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
         }
         else{
             updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
@@ -87,7 +79,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder ='Venue'  name='venue' />
                         <Button
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} floated='right' 
+                            loading={isSubmitting} floated='right' 
                             positive type='submit' content='Submit'/>
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel'/>
                     </Form>
